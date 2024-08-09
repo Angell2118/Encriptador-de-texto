95a80b8a7f16636fbe500f564c7a1caa7ad10fbb
angel <angel.rin@hotmail.com>
2024-08-08 23:00:59 -0600

    agregar etiquetas a html, agregar background_color, declarar funciones, agregar cuadro de insertar texto

---
+++ encriptador.js
@@ -0,0 +1,22 @@
+//variables
+let texto ="";
+let mensajeEncriptado ="";
+
+
+//document.getElementById('texto').value;
+
+
+console.log(texto);
+
+
+//funciones
+function encriptar(){
+
+
+}
+
+function desencriptar(){
+
+
+}
+
